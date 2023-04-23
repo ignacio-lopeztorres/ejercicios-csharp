b836e96bf6a5fa1ed94f4a23727f61fbb8d49916
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-23 17:15:38 -0600

    Uso de dos filtros en la consulta where de linq con la cosnulta de queries

--- CursoLinq/Program.cs
+++ CursoLinq/Program.cs
@@ -15,8 +15,14 @@ var numeroImpares = numeros.Where(x => x % 2 == 1);
 //uso de dos filtros en la consulta de where
 var numeroImparesmayoresQue10 = numeros.Where((x) => x % 2 == 1 && x > 10).ToList(); ;
 
+//consultas de queries
+var numerosImparesMayoresQue10 = from n in numeros
+                                 where n % 2 == 1 && n > 10
+                                 select n;
+
+
 Console.WriteLine("Estos son los numeros impares mayor a 10");
-foreach (var numeroImpar in numeroImparesmayoresQue10)
+foreach (var numeroImpar in numerosImparesMayoresQue10)
 {
     Console.WriteLine(numeroImpar);
 }
