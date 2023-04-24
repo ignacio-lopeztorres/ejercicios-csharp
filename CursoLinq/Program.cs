ab00bddf1083696a1477f293933294cd078cb0b9
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-24 10:43:47 -0600

    filtrado por tipo OfType con queries

--- CursoLinq/Program.cs
+++ CursoLinq/Program.cs
@@ -1,5 +1,6 @@
 ﻿using CursoLinq;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 
 // consultas por typos
 var listado = new List<object>() {"Felipe", 1, 2, "Claudia", true };
@@ -7,5 +8,12 @@ var listado = new List<object>() {"Felipe", 1, 2, "Claudia", true };
 //obtiene el listado de objetos de tipo cadena
 var ObjetosCadena = listado.OfType<string>();
 var numeros = listado.OfType<int>();
+
+//sintaxis con queries
+
+var candenas = from l in listado.OfType<string>()
+                select l;
+var numeros_2 = from l in listado.OfType<int>()
+                select l; 
 
 var a = 1;
