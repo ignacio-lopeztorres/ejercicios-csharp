51e95fbba447c49bcaa8a21776c20d9a2c5f3090
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-26 17:11:30 -0600

    uso de orderby de manera ascendente

--- CursoLinq/Program.cs
+++ CursoLinq/Program.cs
@@ -2,31 +2,17 @@
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 
-// consultas por typos
-var listado = new List<object>() {"Felipe", 1, 2, "Claudia", true };
-
-//obtiene el listado de objetos de tipo cadena
-var ObjetosCadena = listado.OfType<string>();
-var numeros = listado.OfType<int>();
-
-//sintaxis con queries
-
-var candenas = from l in listado.OfType<string>()
-                select l;
-var numeros_2 = from l in listado.OfType<int>()
-                select l;
-
-//Ejemplo 2: Con Herencia
-//
-var ListadoAnimales = new List<Animal>(){
-    new Perro() { Name ="Firulais" },
-    new Gato() { Name = "Félix" }
+var personas = new List<Persona>() {
+    new Persona { Nombre = "Eduardo", Edad = 30, FechaIngresoALaEmpresa = new DateTime(2021, 1, 2), Soltero = true },
+    new Persona { Nombre = "Nidia", Edad = 19, FechaIngresoALaEmpresa = new DateTime(2015, 11, 22), Soltero = true },
+    new Persona { Nombre = "Alejandro", Edad = 45, FechaIngresoALaEmpresa = new DateTime(2020, 4, 12), Soltero = false },
+    new Persona { Nombre = "Valentina", Edad = 24, FechaIngresoALaEmpresa = new DateTime(2021, 7, 8), Soltero = false },
+    new Persona { Nombre = "Roberto", Edad = 61, FechaIngresoALaEmpresa = DateTime.Now.AddDays(-1), Soltero = false },
 };
 
-//extrae determinadas clases derivadas de una clase abstracta
-var perros = ListadoAnimales.OfType<Perro>();
-// OfType es util por ejemplo cuando se utiliza herencia con SQLServer FrameworkCore
-//donde se se puede extraer con un query una caterioria especifica de una entidad en una base de datos
-
-
-var a = 1;
+//ordena de manera ascendente
+var personasOrdenadasPorEdad = personas.OrderBy(p => p.Edad);
+foreach (var persona in personasOrdenadasPorEdad)
+{
+    Console.WriteLine($"{persona.Nombre} tiene {persona.Edad} años de edad");
+}
