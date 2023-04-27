30b758c568fe29707b35732837e835bd0448234b
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-27 13:38:59 -0600

    Uso de Sum, Max, Mim

--- CursoLinq/Program.cs
+++ CursoLinq/Program.cs
@@ -4,29 +4,20 @@ using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
 
 var personas = new List<Persona>() {
-    new Persona { Nombre = "Eduardo", Soltero = true },
-    new Persona { Nombre = "Nidia", Soltero = true },
-    new Persona { Nombre = "Alejandro", Soltero = true },
-    new Persona { Nombre = "Valentina", Soltero = false}
+    new Persona { Nombre = "Eduardo", Edad = 19 },
+    new Persona { Nombre = "Nidia", Edad = 25 },
+    new Persona { Nombre = "Alejandro", Edad = 30 },
+    new Persona { Nombre = "Valentina", Edad = 22}
 };
 
+var numeros = Enumerable.Range(1, 5);
 
-//uso de count para contar elementos de una lista
-Console.WriteLine($"La cantidad de personas es: {personas.Count()}");
+//suma los elementos de numeros
+Console.WriteLine($"La suma de los numeros es: {numeros.Sum()}");
 
-//uso de cou t con predicados para campos especificos
-Console.WriteLine($"La cantidad de personas soltera es: {personas.Count(p => p.Soltero)}");
+//suma las edades de las personas
+Console.WriteLine($"La Suma de las Edades es: {personas.Sum(p => p.Edad)}");
 
-//Valor Maximo de int
-Console.WriteLine($"Int max = {int.MaxValue.ToString("N")}");
+Console.WriteLine($"La edad maxima de las personas es: {personas.Max(x => x.Edad)}");
+Console.WriteLine($"La edad minima de las personas es: {personas.Min(x => x.Edad)}");
 
-//Cuando el valor maximo de datos sea mayor a 3 millones se recomienda usar
-//LongCount();
-//personas.LongCount();
-
-//para el uso de queries con count no vale mcho la pena porque se usa mas codico que con el uso de metodos
-//uso de la sintaxis de queries
-
-var personasSolteras = (from p in personas
-                        where p.Soltero
-                        select p).Count();
