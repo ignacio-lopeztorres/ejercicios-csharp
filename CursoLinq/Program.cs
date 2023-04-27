4aac976aef02f960342dabad7b0ae77899a728ec
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-27 14:12:57 -0600

    uso de la funcion agregate para realizar operaciones entre cada uno de los elemento esperando un valor final

--- CursoLinq/Program.cs
+++ CursoLinq/Program.cs
@@ -1,19 +1,15 @@
 ﻿using CursoLinq;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography.X509Certificates;
-
-var personas = new List<Persona>() {
-    new Persona { Nombre = "Eduardo", Edad = 19 },
-    new Persona { Nombre = "Nidia", Edad = 25 },
-    new Persona { Nombre = "Alejandro", Edad = 30 },
-    new Persona { Nombre = "Valentina", Edad = 22}
-};
 
 var numeros = Enumerable.Range(1, 5);
 
-//promedio de los elementos de numeros
-Console.WriteLine($"El promedio de los numeros es: {numeros.Average()}");
+//con la funcion de agregado podemos realizar operaciones entre cada uno de los elementos
+//de la coleccion mientras calculamos el valor final
 
-//promedio de las edades de las personas
-Console.WriteLine($"El promedio de las Edades es: {personas.Average(p => p.Edad)}");
+var resultado = numeros.Aggregate((a, b) => a * b); //realiza la operacion de 1x2x3x4x5 dando como resultado 120
+
+Console.WriteLine($"El resultado es: {resultado}");
+
+//realiza la operacion con un valor semilla
+var resultadoConValorSemilla = numeros.Aggregate(10, (a, b) => a * b);
+
+Console.WriteLine($"El resulado con el valor semilla es: {resultadoConValorSemilla}");
