fb7143c9c77e796706f91af259f65469c0efdbae
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-28 13:56:58 -0600

    Uso de GroupJoin que funciona como left join en SQL

---
+++ GroupJoin-LeftJoin/Empresa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupJoin_LeftJoin
+{
+    public class Empresa
+    {
+        public Empresa() { }
+        public Empresa(int id) { }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GroupJoin-LeftJoin/Program.cs
@@ -0,0 +1,33 @@
+﻿using GroupJoin_LeftJoin;
+
+var personas = new List<Persona>(){
+    new Persona { Name = "Eduardo", EmpresaId = 1 },
+    new Persona { Name = "Nidia", EmpresaId = 1 },
+    new Persona { Name = "Alejandro", EmpresaId = 3 },
+    new Persona { Name = "Valentina", EmpresaId = 2 },
+    new Persona { Name = "Roberto", EmpresaId = 3 },
+    new Persona { Name = "Eugenia" },
+    new Persona { Name = "Esmerlin", EmpresaId = 3 },
+};
+
+var empresas = new List<Empresa>() {
+    new Empresa { Id = 1, Name = "Electrodomesticos felipe"},
+    new Empresa { Id = 2, Name = "Bicicletas Vanlentina"},
+    new Empresa { Id = 3, Name = "Gimnasio Esmerlin"},
+    new Empresa { Id = 4, Name = "Ferreteria Lorenzo"}
+};
+
+var empresasYEmpleados = empresas.GroupJoin(personas, e => e.Id, p => p.EmpresaId, (empresa, persona) => new { 
+    Persona = persona,
+    Empresa = empresa
+});
+
+foreach (var item in empresasYEmpleados) { 
+    Console.WriteLine($"Las Siguientes personas trabajan en {item.Empresa.Name}");
+    foreach (var persona in item.Persona)
+    {
+        Console.WriteLine($"- {persona.Name}");
+    }
+}
+
+
