1f62bfd66ad91414f61521e439d0fc57615a0f68
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-28 11:13:50 -0600

    agrupamiento de elementos por edad usuando queires

--- CursoLinq/Program.cs
+++ CursoLinq/Program.cs
@@ -13,8 +13,13 @@ var personas = new List<Persona>() {
 //agrupar personas solteras y no solteras
 var agrupaPorRangoDeEdad = personas.GroupBy(p => p.Edad / 5);
 
+//Sintaxis de queries
+var agrupaPorRangoDeEdad_2 = from p in personas
+                             group p by p.Edad /5 into grupos
+                             select grupos;
 
-foreach (var grupo in agrupaPorRangoDeEdad)
+
+foreach (var grupo in agrupaPorRangoDeEdad_2)
 {
     Console.WriteLine($"Grupo de las personas en el rango de edad {grupo.Key * 5} (Total: {grupo.Key * 5 + 5 - 1})");
     foreach (var persona in grupo)
