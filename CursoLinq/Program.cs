a0f13a2e1ffc64051ecc56d8372309616a59168f
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-27 18:27:35 -0600

    Uso de Skip en combinacion de Take para saltar y tomar cantidad de elemento que se les indique en sus parametros

--- CursoLinq/Program.cs
+++ CursoLinq/Program.cs
@@ -5,14 +5,11 @@ var numeros = Enumerable.Range(1, 100);
 //obtiene los 10 primero numeros
 var primeros10Numero = numeros.Take(10).ToList();
 
-//obtiene los 10 ultimos numeros
-var ultimos10Numeros = numeros.TakeLast(10).ToList(); ;
+//uso de Skip, el cuan indica que se salte la cantidad de elementos y se usa en conbinacion de take, por ejemplo
+var segundoLoteDe10 = numeros.Skip(10).Take(10).ToList();
 
-//no vale la pena usar take y takeLAst con queries ya que se escribiria mas codigo en la sintaxis de queries
-//por ejemplo
-//Sintaxis de queries
-var primeros10Numero_2 = (from n in numeros
-                          select n).Take(10).ToList();
-var ultimos10Numero_2 = (from n in numeros
-                          select n).TakeLast(10).ToList();
+//obtiene los 10 ultimos numeros
+var ultimos10Numeros = numeros.TakeLast(10).ToList();
+var penultimoLote = numeros.SkipLast(10).TakeLast(10).ToList();
+
 var a = 1;
