a968e7ab2bbd22848f339d66bb53abb405a4a640
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-28 12:32:37 -0600

    Uso de join para relacionar dos colecciones por medio de un campo en comun

---
+++ Join/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Join
+{
+    public class Persona
+    {
+        public Persona() { }
+        public Persona(int id) { }
+        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public int EmpresaId { get; set; }
+        
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string PostalCode { get; set; }
+        public string CountryCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PostalCodeCode { get; set; }
+        public string CountryCodeCode { get; set;}
+        public string PhoneNumberCode { get; set; }
+        
+    }
+}
---
+++ Join/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Join;
+using System.Timers;
+
+var personas = new List<Persona>(){
+    new Persona { Name = "Eduardo", EmpresaId = 1 },
+    new Persona { Name = "Nidia", EmpresaId = 1 },
+    new Persona { Name = "Alejandro", EmpresaId = 3 },
+    new Persona { Name = "Valentina", EmpresaId = 2 },
+    new Persona { Name = "Roberto", EmpresaId = 3 },
+    new Persona { Name = "Eugenia" },
+    new Persona { Name = "Esmerlin", EmpresaId = 3 },
+};
+
+var empresas = new List<Empresa>() {
+    new Empresa { Id = 1, Name = "Electrodomesticos felipe"},
+    new Empresa { Id = 2, Name ="Bicicletas Vanlentina"},
+    new Empresa { Id = 3, Name = "Gimnasio Esmerlin"}
+};
+
+//Realiza la union entre personas y empresas
+//El primer paametros de join indica que la coleccionde personas a que coleccion se uniran, en este caso empresa
+//el segundo parametro indica el camppo de personas que se usara para hacer la union
+// El tercer Parametro indica que campo de empresa se usara para unir con la coleccion de persona
+//ElapsedEventArgs tercer parametro indica que retorna un objeto anonimo con la union de las dos colecciones, la proyeccionde los resultados
+var personasYEmpresas = personas.Join(empresas, p => p.EmpresaId, e => e.Id, (persona,empresa) => new { 
+    Persona = persona,
+    Empresa = empresa
+});
+
+
+foreach (var item in personasYEmpresas)
+{
+    Console.WriteLine($"{item.Persona.Name} trabaja en {item.Empresa.Name}");
+    
+}
