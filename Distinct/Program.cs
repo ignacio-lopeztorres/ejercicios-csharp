71d9a7e7df2439cd9ea6b1702949a73d88865978
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-28 16:30:21 -0600

    Elimina elementos repetidos en una colecion con Distinct y DistinctBy

---
+++ Distinct/Program.cs
@@ -0,0 +1,18 @@
+﻿using Distinct;
+
+var personas = new List<Persona>() {
+    new Persona { Name = "Eduardo" , EmpresaID = 1 },
+    new Persona { Name = "Nidia" , EmpresaID = 1 },
+    new Persona { Name = "Eduardo" },
+    new Persona { Name = "Esmerlin" , EmpresaID = 3 }
+};
+
+//Dsictinct se usa para eliminar elementos reeptidos dentro de una collecion
+int[] numeros = { 1, 2, 3, 1, 1, 6 };
+
+var numerosSiRepeticiones = numeros.Distinct().ToArray();
+
+var personasSinNombreRepetidos = personas.DistinctBy(p => p.Name );
+
+var a = 1;
+
