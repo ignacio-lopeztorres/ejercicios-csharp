08cff391000f124f592df9e9c075347d92da76b2
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-28 19:39:24 -0600

    uniones entre dos colecciones

---
+++ Union/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Union
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int EmpresaID { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ Union/Program.cs
@@ -0,0 +1,19 @@
+﻿using Union;
+
+var personasA = new List<Persona>() {
+    new Persona { Name = "Eduardo" , EmpresaID = 1 },
+    new Persona { Name = "Nidia" , EmpresaID = 1 },
+    new Persona { Name = "Esmerlin" , EmpresaID = 3 }
+};
+
+var personasB = new List<Persona>() {
+    new Persona { Name = "Fernando" , Age = 57 },
+    new Persona { Name = "Nidia" , EmpresaID = 1 }
+};
+
+int[] numerosA = { 1, 2, 3, 1, 1, 6 };
+
+int[] numeroB = { 1, 2, 15 };
+
+//une las dos coleciones de numeros donde cada elemento que se repite ya no ingresa a la nueva coleccion
+var unionNumero = numerosA.Union(numeroB);
