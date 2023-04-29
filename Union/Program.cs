b8360df5aa09c3e0d4bb41559acd3bf32ac96f41
Ignacio lopez <ignacio.lopeztorres@hotmail.com>
2023-04-28 20:26:51 -0600

    Uso de ExceptBy

--- Union/Program.cs
+++ Union/Program.cs
@@ -11,15 +11,27 @@ var personasB = new List<Persona>() {
     new Persona { Name = "Nidia" , EmpresaID = 1 }
 };
 
-int[] numerosA = { 1, 2, 3, 1, 1, 6 };
+//int[] numerosA = { 1, 2, 3, 1, 1, 6 };
 
-int[] numeroB = { 1, 2, 15 };
+//int[] numeroB = { 1, 2, 15 };
 
-//une las dos coleciones de numeros donde cada elemento que se repite ya no ingresa a la nueva coleccion
-var unionNumero = numerosA.Union(numeroB);
+////une las dos coleciones de numeros donde cada elemento que se repite ya no ingresa a la nueva coleccion
+//var unionNumero = numerosA.Union(numeroB);
 
-//union de dos colecciones de objetos usando unionBy
+////union de dos colecciones de objetos usando unionBy
 
-var unionPersonas = personasA.UnionBy(personasB, p => p.Name);
+//var unionPersonas = personasA.UnionBy(personasB, p => p.Name);
+int[] A = { 1, 2, 3 };
+int[] B = { 1, 15};
+
+//Muestra los Elementos en A que no estan en B, el cual en est caso el resultado es:
+//{ 2, 3 }
+var numerosEnAQueNoEstanEnB = A.Except(B);
+
+//selecciona tos los nombres de la coleccion personasB
+var personasBNombre = personasB.Select(p => p.Name);
+//el primer parametro es donde indica la coleccion a comparar y el segundo parametro es de la collecion personasA A comparar
+var PersonasEnAQueNoEstanEnB = personasA.ExceptBy(personasBNombre, x => x.Name);
+
 
 var a = 1; //breackpoint
